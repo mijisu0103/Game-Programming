d5f107ba7011833eae08a9d4a9fd2a2b4eeda05f
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-14 03:28:20 +0100

    Define.h

--- Break out game/Define.h
+++ Break out game/Define.h
@@ -1,14 +1,27 @@
-#pragma once // Prevents duplicate header files when there are many files,
-// which can cause errors. You can just leave it as is.
-
+Define.h
+#pragma once
 #include <iostream>
 #include <Windows.h>
-#include <time.h>
 #include <conio.h>
 using namespace std;
 
-struct INFO // Ball
+struct BALL
 {
-	int m_nX;
-	int m_nY;
+	int nReady;
+	int nX, nY;
+	int nDirect;
+};
+static void gotoxy(int x, int y)
+{
+	COORD CursorPosition = { x,y };
+	SetConsoleCursorPosition(GetStdHandle
+	(STD_OUTPUT_HANDLE), CursorPosition);
+}
+
+static int g_WallCollision[4][6] =
+{
+	{3 ,  2, -1, -1, -1, 4},
+	{-1, -1, -1, -1,  2, 1},
+	{-1,  5,  4, -1, -1,-1},
+	{-1, -1,  1,  0,  5,-1}
 };
