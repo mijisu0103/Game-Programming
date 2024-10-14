4c576d50d1bdd572ff35a7099b8e304b8e85588d
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-14 03:40:35 +0100

    CBall.cpp

--- Break out game/CBall.cpp
+++ Break out game/CBall.cpp
@@ -1,147 +1,231 @@
 #include "CBall.h"
+#include "CBar.h"
+#include "CBlock.h"
+void CBall::ScreenLine()
+{
+    gotoxy(0, 0);
+    cout << "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓";
+    gotoxy(0, 1);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 2);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 3);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 4);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 5);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 6);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 7);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 8);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 9);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 10);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 11);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 12);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 13);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 14);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 15);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 16);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 17);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 18);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 19);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 20);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 21);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 22);
+    cout << "┃                                                                            ┃";
+    gotoxy(0, 23);
+    cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
+}
 
-CBall::CBall()
+int CBall::Collision(int x, int y)
+{
+    //벽돌
+    //20
+    for (int i = 0; i < 20; i++)
+    {
+        if (m_pBlock->tBlock[i].nLife == 1)
+        {
+            if (x >= m_pBlock->tBlock[i].nX &&
+                x <= m_pBlock->tBlock[i].nX + 1 &&
+                y == m_pBlock->tBlock[i].nY)
+            {
+                //1->2 2->1 5->4   4->5
+                if (m_tBall.nDirect == 1)
+                    m_tBall.nDirect = 2;
+                else if (m_tBall.nDirect == 2)
+                    m_tBall.nDirect = 1;
+                else if (m_tBall.nDirect == 5)
+                    m_tBall.nDirect = 4;
+                else if (m_tBall.nDirect == 4)
+                    m_tBall.nDirect = 5;
+
+                m_pBlock->tBlock[i].nLife = 0;
+                return 1;
+            }
+            else  if (x >= m_pBlock->tBlock[i].nX &&
+                x <= m_pBlock->tBlock[i].nX + 1 &&
+                y == m_pBlock->tBlock[i].nY + 1)
+            {
+                //1->2 2->1 5->4   4->5
+                if (m_tBall.nDirect == 1)
+                    m_tBall.nDirect = 2;
+                else if (m_tBall.nDirect == 2)
+                    m_tBall.nDirect = 1;
+                else if (m_tBall.nDirect == 5)
+                    m_tBall.nDirect = 4;
+                else if (m_tBall.nDirect == 4)
+                    m_tBall.nDirect = 5;
+
+                m_pBlock->tBlock[i].nLife = 0;
+                return 1;
+            }
+        }
+    }
+    
+
+
+
+    //벽충돌
+    if (y == 0)
+    {
+        m_tBall.nDirect = g_WallCollision[0][m_tBall.nDirect];
+        return 1;
+    }
+    if (x == 1)
+    {
+        m_tBall.nDirect = g_WallCollision[1][m_tBall.nDirect];
+        return 1;
+    }
+    if (x == 77)
+    {
+        m_tBall.nDirect = g_WallCollision[2][m_tBall.nDirect];
+        return 1;
+    }
+
+    //바 충돌
+
+    if (x >= m_pBar->m_tBar.nX[0] &&
+        x <= m_pBar->m_tBar.nX[2] + 1 &&
+        y == (m_pBar->m_tBar.nY))
+    {
+        m_tBall.nDirect = g_WallCollision[3][m_tBall.nDirect];
+        return 1;//방향이 바뀐다.
+    }
+    else if (x >= m_pBar->m_tBar.nX[0] &&
+        x <= m_pBar->m_tBar.nX[2] + 1 &&
+        y == m_pBar->m_tBar.nY + 1)
+    {
+        m_tBall.nDirect = g_WallCollision[0][m_tBall.nDirect];
+        return 1;
+    }
+
+
+    if (y == 23)
+    {
+        m_tBall.nDirect = g_WallCollision[3][m_tBall.nDirect];
+        return 1;
+    }
+
+
+
+   
+
+
+    return 0;
+}
+
+void CBall::KeyInput()
 {
 }
 
-CBall::CBall(int Direct)
+void CBall::Initialize()
 {
-    m_tBall.nReady = 0; // Ball is not ready to move
-    m_tBall.nDirect = Direct; // Initial direction
-    m_tBall.nX = 30; // Initial X position
-    m_tBall.nY = 10; // Initial Y position
+    m_tBall.nReady = 0; //0움직인다.
+    m_tBall.nDirect = 2; 
+    m_tBall.nX = 20;
+    m_tBall.nY = 1;
+}
+
+void CBall::Progress()
+{
+    if (m_tBall.nReady == 0)
+    {
+        switch (m_tBall.nDirect)
+        {
+        case 0:
+            if (Collision(m_tBall.nX, m_tBall.nY - 1) == 0)
+                m_tBall.nY--;
+            break;
+        case 1:
+            if (Collision(m_tBall.nX + 1, m_tBall.nY - 1) == 0)
+            {
+                m_tBall.nX++;
+                m_tBall.nY--;
+            }
+            break;
+        case 2:
+            if (Collision(m_tBall.nX + 1, m_tBall.nY + 1) == 0)
+            {
+                m_tBall.nX++;
+                m_tBall.nY++;
+            }
+            break;
+        case 3:
+            if (Collision(m_tBall.nX , m_tBall.nY + 1) == 0)
+            {
+                m_tBall.nY++;
+            }
+            break;
+        case 4:
+            if (Collision(m_tBall.nX-1, m_tBall.nY + 1) == 0)
+            {
+                m_tBall.nX--;
+                m_tBall.nY++;
+            }
+            break;
+        case 5:
+            if (Collision(m_tBall.nX - 1, m_tBall.nY - 1) == 0)
+            {
+                m_tBall.nX--;
+                m_tBall.nY--;
+            }
+            break;
+        }
+    }
+}
+
+void CBall::Render()
+{
+    ScreenLine();
+    gotoxy(m_tBall.nX, m_tBall.nY);
+    cout << "●";
+}
+
+void CBall::Release()
+{
+}
+
+CBall::CBall()
+{
 }
 
 CBall::~CBall()
 {
 }
 
-// Function to draw the walls
-void CBall::ScreenLine()
-{
-    gotoxy(0, 0);
-    cout << "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓";
-    for (int i = 1; i < 23; ++i) {
-        gotoxy(0, i);
-        cout << "┃                                                                            ┃";
-    }
-    gotoxy(0, 23);
-    cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛";
-}
-
-// Function to handle collision detection with walls
-int CBall::Collision(int x, int y)
-{
-    if (y == 0) // Top wall
-    {
-        m_tBall.nDirect = g_WallCollision[0][m_tBall.nDirect]; // Change direction
-        return 1; // Direction changed
-    }
-
-    if (x == 1) // Left wall
-    {
-        m_tBall.nDirect = g_WallCollision[1][m_tBall.nDirect]; // Change direction
-        return 1; // Direction changed
-    }
-
-    if (x == 76) // Right wall
-    {
-        m_tBall.nDirect = g_WallCollision[2][m_tBall.nDirect]; // Change direction
-        return 1; // Direction changed
-    }
-
-    if (y == 23) // Bottom wall
-    {
-        m_tBall.nDirect = g_WallCollision[3][m_tBall.nDirect]; // Change direction
-        return 1; // Direction changed
-    }
-
-    return 0; // No direction change
-}
-
-// Function to handle keyboard input
-void CBall::KeyInput()
-{
-    int nKey;
-    if (_kbhit()) // Check if a key is pressed
-    {
-        nKey = _getch();
-        if (nKey == 32) // Spacebar
-        {
-            m_tBall.nReady = 1; // Set ball to ready state
-        }
-    }
-}
-
-// Function to initialize the ball's properties
-void CBall::Initialize()
-{
-    m_tBall.nReady = 0; // Not moving
-    m_tBall.nDirect = 0; // Initial direction
-    m_tBall.nX = 30; // Initial X position
-    m_tBall.nY = 10; // Initial Y position
-}
-
-// Function to update the ball's position
-void CBall::Progress()
-{
-    if (m_tBall.nReady == 0) // Only if not ready
-    {
-        switch (m_tBall.nDirect)
-        {
-            case 0: // Up
-                if (Collision(m_tBall.nX, m_tBall.nY - 1) == 0)
-                {
-                    m_tBall.nY--;
-                }
-                break;
-            case 1: // Up-Right
-                if (Collision(m_tBall.nX + 1, m_tBall.nY - 1) == 0)
-                {
-                    m_tBall.nX++;
-                    m_tBall.nY--;
-                }
-                break;
-            case 2: // Down-Right
-                if (Collision(m_tBall.nX + 1, m_tBall.nY + 1) == 0)
-                {
-                    m_tBall.nX++;
-                    m_tBall.nY++;
-                }
-                break;
-            case 3: // Down
-                if (Collision(m_tBall.nX, m_tBall.nY + 1) == 0)
-                {
-                    m_tBall.nY++;
-                }
-                break;
-            case 4: // Down-Left
-                if (Collision(m_tBall.nX - 1, m_tBall.nY + 1) == 0)
-                {
-                    m_tBall.nX--;
-                    m_tBall.nY++;
-                }
-                break;
-            case 5: // Up-Left
-                if (Collision(m_tBall.nX - 1, m_tBall.nY - 1) == 0)
-                {
-                    m_tBall.nX--;
-                    m_tBall.nY--;
-                }
-                break;
-        }
-    }
-}
-
-// Function to render the ball on the screen
-void CBall::Render()
-{
-    ScreenLine();
-    gotoxy(m_tBall.nX, m_tBall.nY);
-    cout << "●"; // Draw the ball
-}
-
-// Cleanup function (currently empty)
-void CBall::Release()
-{
-}
