983644f644db618da6b28cac25a19c849d0a58a2
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-14 03:39:55 +0100

    CBall.h

--- Break out game/CBall.h
+++ Break out game/CBall.h
@@ -1,13 +1,26 @@
 #pragma once
-#include "Define.h"
+#include "define.h"
+class CBar;
+class CBlock;
 class CBall
 {
 private:
 	BALL m_tBall;
+	//MainGame
+	CBar* m_pBar;
+	CBlock* m_pBlock;
 public:
+	void SetBar(CBar* pBar) {m_pBar = pBar;	}
+	void SetBlock(CBlock* pBlock) { m_pBlock = pBlock; }
 	void ScreenLine();
 	int Collision(int x, int y);
 	void KeyInput();
+public:
+	BALL GetBall() { return m_tBall; }
+	void SetX(int x) { m_tBall.nX += x; }
+	void SetY(int y) { m_tBall.nY += y; }
+	void SetBall(BALL tBall) { m_tBall = tBall; }
+	void SetReady(int Ready) { m_tBall.nReady = Ready; }
 public:
 	void Initialize();
 	void Progress();
@@ -15,7 +28,7 @@ public:
 	void Release();
 public:
 	CBall();
-	CBall(int Direct);
 	~CBall();
 };
+
 
