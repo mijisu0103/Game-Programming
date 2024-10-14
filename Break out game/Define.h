2d5f7c45ce92886045988c6a57a3700d1c25d9fb
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-14 03:39:38 +0100

    Define.h

--- Break out game/Define.h
+++ Break out game/Define.h
@@ -1,27 +1,43 @@
-Define.h
 #pragma once
-#include <iostream>
 #include <Windows.h>
 #include <conio.h>
+#include <time.h>
+#include <iostream>
 using namespace std;
+
+#define LEFT 75
+#define RIGHT 77
+
+struct BAR
+{
+	int nX[3];
+	int nY;
+};
 
 struct BALL
 {
 	int nReady;
-	int nX, nY;
 	int nDirect;
+	int nX, nY;
 };
-static void gotoxy(int x, int y)
+
+struct BLOCK
 {
-	COORD CursorPosition = { x,y };
-	SetConsoleCursorPosition(GetStdHandle
-	(STD_OUTPUT_HANDLE), CursorPosition);
-}
+	int nLife;
+	int nX, nY;
+};
+
 
 static int g_WallCollision[4][6] =
 {
-	{3 ,  2, -1, -1, -1, 4},
-	{-1, -1, -1, -1,  2, 1},
-	{-1,  5,  4, -1, -1,-1},
-	{-1, -1,  1,  0,  5,-1}
+	{ 3,  2, -1, -1, -1,  4},
+	{-1, -1, -1, -1,  2,  1},
+	{-1,  5,  4, -1, -1, -1},
+	{-1, -1,  1,  0,  5, -1}
 };
+static void gotoxy(int x, int y)
+{
+	COORD CursorPosition = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE)
+		, CursorPosition);
+}
