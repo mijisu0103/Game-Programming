154da655b132ee0532ea752e1d6238ffb8e79b5f
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-14 03:23:58 +0100

    Define.h

---
+++ Break out game/Define.h
@@ -0,0 +1,14 @@
+#pragma once // Prevents duplicate header files when there are many files,
+// which can cause errors. You can just leave it as is.
+
+#include <iostream>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
+using namespace std;
+
+struct INFO // Ball
+{
+	int m_nX;
+	int m_nY;
+};
