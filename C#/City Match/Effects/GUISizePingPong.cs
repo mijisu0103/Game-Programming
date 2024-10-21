759887ec22bc5801ea1e4e099eae41ed2a4b96c6
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-21 17:13:56 +0100

    GUISizePingPong.cs

---
+++ C#/City Match/Effects/GUISizePingPong.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class GUISizePingPong : MonoBehaviour {
+
+	public Vector3 minScale = new Vector3(.5f, .5f, .5f);
+	public float changeDelay = .03f;
+	RectTransform rectTransform;
+
+	void Start() {
+		rectTransform = GetComponent<RectTransform>();
+		StartCoroutine(PingPongSize());
+	}
+
+	// Change size down to minScale then back up to max scale
+	IEnumerator PingPongSize() {
+		Vector3 startingScale = rectTransform.localScale;
+		Vector3 currentScale = startingScale;
+		
+		// How much to change the scale each iteration
+		Vector3 changeScale = new Vector3(.01f, .01f, .01f);
+		bool shrink = true;
+
+		while (true) {
+			if (shrink) {
+				currentScale -= changeScale;
+				if (currentScale.x <= minScale.x) {
+					shrink = false;
+				}
+			} else {
+				currentScale += changeScale;
+				if (currentScale.x >= 1) {
+					shrink = true;
+				}
+            }
+			rectTransform.localScale = currentScale;
+			yield return new WaitForSeconds(changeDelay);
+		}
+	}
+}
